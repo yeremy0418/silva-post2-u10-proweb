12645c95e444eb39cf688e96ceb6fc99dcd3697b
yeremy0418 <145410510+yeremy0418@users.noreply.github.com>
2026-05-12 16:26:59 -0500

    Reapply "Add Postman collection and environment files for ToDoApp API testing"
    This reverts commit f9ba16bb22497eee0cd683eede79cf16045a93ab.

--- src/main/java/com/tareas/gestion_tareas/controller/TareaController.java
+++ src/main/java/com/tareas/gestion_tareas/controller/TareaController.java
@@ -2,11 +2,17 @@ package com.tareas.gestion_tareas.controller;
 
 import com.tareas.gestion_tareas.model.Tarea;
 import com.tareas.gestion_tareas.service.TareaService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/tareas")
@@ -22,5 +28,17 @@ public class TareaController {
     public ResponseEntity<Tarea> buscarPorId(@PathVariable Long id) {
         Tarea tarea = service.buscarPorId(id);
         return ResponseEntity.ok(tarea);
+    }
+
+    @PostMapping
+    public ResponseEntity<Tarea> crear(@Valid @RequestBody Tarea tarea) {
+        Tarea creada = service.crear(tarea);
+        return ResponseEntity.status(HttpStatus.CREATED).body(creada);
+    }
+
+    @PatchMapping("/{id}/completar")
+    public ResponseEntity<Tarea> completar(@PathVariable Long id) {
+        Tarea completada = service.completar(id);
+        return ResponseEntity.ok(completada);
     }
 }
